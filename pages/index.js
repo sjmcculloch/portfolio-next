15cbc08a87ca9ca9e56c213eb87bc001acdfb8d0
Scott McCulloch <smcculloch@gmail.com>
2019-01-09 08:08:05 +1100

    styles updates

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,9 @@
 import React, { Component } from "react";
-import axios from "axios";
+import { Button, Container } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import BaseLayout from "../components/layouts/BaseLayout";
 
 class Index extends Component {
-  static async getInitialProps() {
-    let userData = {};
-    try {
-      const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/todos/1"
-      );
-      userData = response.data;
-    } catch (err) {
-      console.log(err);
-    }
-    return { initialData: [1, 2, 3, 4], userData };
-  }
   constructor(props) {
     super(props);
 
@@ -23,26 +12,12 @@ class Index extends Component {
     };
   }
 
-  componentDidMount() {}
-
-  componentDidUpdate() {}
-
-  componentWillUnmount() {}
-
-  updateTitle = () => {
-    this.setState({ title: "I am updated Index Page" });
-  };
-
   render() {
-    const { title } = this.state;
-
-    const { userData } = this.props;
     return (
       <BaseLayout>
-        <h1>I am a header subtitle</h1>
-        <h2>{title}</h2>
-        <h2>{userData.title}</h2>
-        <button onClick={this.updateTitle}>Change Title</button>
+        <Container>
+          <Button color="danger">Button</Button>
+        </Container>
       </BaseLayout>
     );
   }
