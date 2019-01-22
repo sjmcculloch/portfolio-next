cdf6380168a746663c4121199c222cb9b1fece17
Scott McCulloch <smcculloch@gmail.com>
2019-01-23 08:17:26 +1100

    mongo updates

--- server/index.js
+++ server/index.js
@@ -11,6 +11,9 @@ const app = next({ dev });
 const handle = routes.getRequestHandler(app);
 const config = require("./config");
 
+const Book = require("./models/book");
+const bodyParser = require("body-parser");
+
 const secretData = [
   {
     title: "SecretData 1",
@@ -41,6 +44,63 @@ app
   .then(() => {
     const server = express();
 
+    server.use(bodyParser.json());
+
+    server.post("/api/v1/books", (req, res) => {
+      const bookData = req.body;
+      const book = new Book(bookData);
+
+      book.save((err, createdBook) => {
+        if (err) {
+          return res.status(422).send(err);
+        }
+
+        return res.json(createdBook);
+      });
+    });
+
+    server.get("/api/v1/books", (req, res) => {
+      Book.find({}, (err, allBooks) => {
+        if (err) {
+          return res.status(422).send(err);
+        }
+
+        return res.json(allBooks);
+      });
+    });
+
+    server.patch("/api/v1/books/:id", (req, res) => {
+      const bookId = req.params.id;
+      const bookData = req.body;
+
+      Book.findById(bookId, (err, foundBook) => {
+        if (err) {
+          return res.status(422).send(err);
+        }
+
+        foundBook.set(bookData);
+        foundBook.save(err => {
+          if (err) {
+            return res.status(422).send(err);
+          }
+
+          return res.json(foundBook);
+        });
+      });
+    });
+
+    server.delete("/api/v1/books/:id", (req, res) => {
+      const bookId = req.params.id;
+
+      Book.deleteOne({ _id: bookId }, (err, deletedBook) => {
+        if (err) {
+          return res.status(422).send(err);
+        }
+
+        return res.json({ status: "DELETED" });
+      });
+    });
+
     server.get("/api/v1/secret", authService.checkJWT, (req, res) => {
       return res.json(secretData);
     });
@@ -50,7 +110,6 @@ app
       authService.checkJWT,
       authService.checkRole("admin"),
       (req, res) => {
-        console.log(req.user);
         return res.json(secretData);
       }
     );
